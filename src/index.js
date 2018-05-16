ee18a1a34955f3febde44d5385a156538b9ffbb0
Eric Fuller <info@glassmoonmedia.com>
2018-05-15 21:39:11 -0400

    Configure eslint and wipe the slate clean.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
-import Sidebar from './components/sidebar';
-import Random from './components/random';
-import Cookie from './components/cookies';
-window.onload = function() {
-	Sidebar.init();
-	Random.init();
-	Cookie();
-};
+
 
 if (module.hot) {
 	module.hot.accept();
