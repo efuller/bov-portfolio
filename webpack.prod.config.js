86baea433d2663419ab9efefab0184911b4d7118
Eric Fuller <info@glassmoonmedia.com>
2018-05-29 21:27:41 -0400

    Prod adjustments.

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -11,7 +11,6 @@ const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const autoprefixer = require('autoprefixer');
 
 /**
  * Webpack config object.
@@ -19,17 +18,17 @@ const autoprefixer = require('autoprefixer');
 const config = {
 	// The entry point of our app. Where our dependency graph starts.
 	entry: [
-		path.resolve(__dirname, 'src/scss/index.scss' ),
-		path.resolve(__dirname, 'src/index.js')
+		path.resolve(__dirname, 'src/scss/index.scss'),
+		path.resolve(__dirname, 'src/index.js'),
 	],
 	// The output location for our compiled bundle.
 	output: {
 		filename: 'js/bundle.js',
 		path: path.resolve(__dirname, './build'),
-		publicPath: '/'
+		publicPath: '/',
 	},
 	resolve: {
-		extensions: ['.js', '.scss']
+		extensions: ['.js', '.scss'],
 	},
 	// Loaders. These transform our code by running them through transpilers.
 	module: {
@@ -37,47 +36,46 @@ const config = {
 			{
 				test: /\.js$/,
 				exclude: /node_modules/,
-				loader: "babel-loader",
+				loader: 'babel-loader',
 			},
 			{
 				test: /\.scss$/,
 				use: ExtractTextPlugin.extract({
 					fallback: 'style-loader',
-					//publicPath: '/md-projects/assets/css/',
-					use: ['css-loader', 'postcss-loader', 'sass-loader']
-				})
+					use: ['css-loader', 'sass-loader'],
+				}),
+			},
+			{
+				test: /\.hbs$/,
+				loader: 'handlebars-loader',
+				query: {
+					partialDirs: [
+						path.join(__dirname, './src/views', 'partials'),
+					],
+				},
 			},
 			{
 				test: /\.(png|jpg|svg)$/,
 				loader: 'file-loader',
 				options: {
 					name: 'images/[name].[ext]',
-					publicPath: '/bov-portfolio/'
-
-				}
-			}
-		]
+					publicPath: '/bov-portfolio/',
+				},
+			},
+		],
 	},
 	// Webpack plugins - These are like addons that can provide additional functionality.
 	plugins: [
 		new webpack.HotModuleReplacementPlugin(),
 		new webpack.NoEmitOnErrorsPlugin(),
 		new webpack.NamedModulesPlugin(),
-		new ExtractTextPlugin( './css/style.css' ),
-		// Configure autoprefixer.
-		new webpack.LoaderOptionsPlugin({
-			options: {
-				postcss: [
-					autoprefixer(),
-				]
-			}
-		}),
+		new ExtractTextPlugin('./css/style.css'),
 		new CleanWebpackPlugin(['build'], {
 			root: __dirname,
 			verbose: true,
 			dry: false,
-			exclude: ['index.html', 'vendor', 'images']
-		})
+			exclude: ['index.html', 'vendor', 'images'],
+		}),
 	],
 };
 
